17550ef5262cf6675910650312e911725bf05d46
Matteo Angelino <matteo.angelino@casa.it>
2016-07-26 19:29:43 +0200

    Update test suite

--- test/test.py
+++ test/test.py
@@ -10,8 +10,7 @@ class json_converter_test(unittest.TestCase):
     
     def test_conversion(self):
         result_data = {}
-        result = jc.convert(source_data,result_data) 
-        print json.dumps(result_data, indent=4)   
+        result = jc.convert(source_data,result_data)    
         self.assertEqual(result,True)
 
     def test_int_field(self):
@@ -29,7 +28,24 @@ class json_converter_test(unittest.TestCase):
         result = jc.convert(source_data,result_data)    
         self.assertEqual(json_converter.JsonWalker.walkto(result_data,['field','string','value']),"TESTNAME") 
 
+    def test_listField_field(self):
+        result_data = {}
+        result = jc.convert(source_data,result_data)    
+        self.assertEqual(json_converter.JsonWalker.walkto(result_data,['field','list_field']),[{'type':'image','file':'1.jpg'},{'type':'image','file':'2.jpg'}])
+
+    def test_toListField_field(self):
+        result_data = {}
+        result = jc.convert(source_data,result_data)    
+        self.assertEqual(json_converter.JsonWalker.walkto(result_data,['field','list','elements']),[{'match' : {'value':'value1'}},{'match' : {'value':'value2'}}])
 
 if __name__ == '__main__':
+    result_data = {}
+    result = jc.convert(source_data,result_data)    
+    print '*** ORIGINAL JSON ***'
+    print json.dumps(source_data, indent=4)
+    print '*** MAPPED JSON ***'
+    print json.dumps(result_data, indent=4)
+
+    print '*** TEST ***'
     suite_1 = unittest.TestLoader().loadTestsFromTestCase(json_converter_test)
     unittest.TextTestRunner(verbosity=2).run(suite_1)
