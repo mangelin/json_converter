9e5f242a7ffd2b458f0f23316d8d0c500d0fe3f0
Matteo Angelino <matteo.angelino@casa.it>
2016-09-07 10:08:41 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='JsonConverter',
+      version='0.1',
+      description='Python Json Converter',
+      author='Matteo Angelino',
+      author_email='matteo.angelino@gmail.com',
+      url='https://github.com/mangelin/json_converter',
+      packages=['json_converter'],
+     )
